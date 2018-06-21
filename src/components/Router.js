8082fe06ffcae778322407fa42a14fc41ea6bd0e
Elliot Jane Lader <elliot.j.lader@gmail.com>
2018-06-21 13:09:20 -0600

    Push with chartjs up and running

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from './App';
+import Bar from './Bar';
 import NotFound from './NotFound';
 
 const Router = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={App} />
+            <Route exact path="/bar" component={Bar} />
             <Route component={NotFound} />
         </Switch>
 </BrowserRouter>
